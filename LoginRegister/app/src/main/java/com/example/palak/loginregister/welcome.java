21c9466882c3f55f6ce65459c8638abbd1396dce
smarty1palak <smarty1palak@gmail.com>
2018-03-09 17:59:29 +0530

    welcome

--- LoginRegister/app/src/main/java/com/example/palak/loginregister/welcome.java
+++ LoginRegister/app/src/main/java/com/example/palak/loginregister/welcome.java
@@ -1,7 +1,10 @@
 package com.example.palak.loginregister;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class welcome extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        final TextView tvRegisterLink = (TextView) findViewById(R.id.tvregister);
+        final TextView tvLoginLink = (TextView) findViewById(R.id.tvlogin);
+        tvRegisterLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(welcome.this, RegisterActivity.class);
+                welcome.this.startActivity(registerIntent);
+            }
+        });
+        tvLoginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(welcome.this, LoginActivity.class);
+                welcome.this.startActivity(loginIntent);
+            }
+        });
     }
 }
