d97cd8f99585bfbad2f0141e43497c0e1e740bb0
Sharanya Kamath <sherrykamath@gmail.com>
2018-03-15 19:23:08 +0530

    changed layout

--- lms123/app/src/main/java/edu1/csumb/mike/Library_System/ProfilePage.java
+++ lms123/app/src/main/java/edu1/csumb/mike/Library_System/ProfilePage.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class ProfilePage extends AppCompatActivity implements View.OnClickListener{
+public class ProfilePage extends AppCompatActivity{
 
 
     MySQLiteHelper db = new MySQLiteHelper(this);
